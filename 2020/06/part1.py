ce75e1b4d0ab5b302ab4a9e4f48e54ff9c162012
Lunik <lunik@tiwabbit.fr>
2020-12-08 23:45:37 +0100

    2020/06

---
+++ 2020/06/part1.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+def parseGroups(data):
+  return list(map(lambda g: set(g.replace('\n', '')), data.split('\n\n')))
+
+def verify(groups):
+  return reduce(lambda a, b: (len(a) if type(a) is set else a) + len(b), groups)
+
+if __name__ == "__main__":
+  with open('input.txt', 'r') as f:
+    groups = parseGroups(f.read())
+
+  print(verify(groups))
