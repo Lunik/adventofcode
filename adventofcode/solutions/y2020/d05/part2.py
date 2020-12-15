0436d9136383c6787aaaceb19fbe83b8d83f3f96
Lunik <lunik@tiwabbit.fr>
2020-12-15 19:30:16 +0100

    2020/05 fix output

--- adventofcode/solutions/y2020/d05/part2.py
+++ adventofcode/solutions/y2020/d05/part2.py
@@ -1,7 +1,7 @@
 import os
 
 
-from adventofcode.solutions.y2020.d05.part1 import parse_seat, get_seat_position
+from adventofcode.solutions.y2020.d05.part1 import parse_seat, get_seat_position, get_postion_id
 
 
 def print_plane(plane):
@@ -10,6 +10,18 @@ def print_plane(plane):
   for row in plane:
     print(count, '\t', ' '.join(row))
     count += 1
+
+
+def find_seat(plane):
+
+  for row in range(1, 128):
+    for column in range(1, 8):
+      if plane[row][column] == '_' \
+        and plane[row][column - 1] == 'X' \
+        and plane[row][column + 1] == 'X':
+        return (row, column)
+
+  return None
 
 
 def main():
@@ -26,7 +38,8 @@ def main():
 
       plane[position[0]][position[1]] = "X"
 
-  return plane
+  seat_position = find_seat(plane)
+  return get_postion_id(seat_position)
 
 
 if __name__ == "__main__":
