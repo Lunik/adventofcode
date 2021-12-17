50a2c39e8614cc6ab64153f64b87e42e9d69b752
Lunik <lunik@tiwabbit.fr>
2021-12-18 00:16:42 +0100

    2021/02
    Signed-off-by: Lunik <lunik@tiwabbit.fr>

---
+++ adventofcode/solutions/y2021/d02/part2.py
@@ -0,0 +1,15 @@
+import os
+
+
+def main():
+  position = [0,0]
+
+  with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as file:
+    for line in file:
+      pass
+
+  return "TODO"
+
+
+if __name__ == "__main__":
+  print(main())
