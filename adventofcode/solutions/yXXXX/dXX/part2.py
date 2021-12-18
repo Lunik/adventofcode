fc8c9b38de1602d51cec969f8e323f97d1563265
Lunik <lunik@tiwabbit.fr>
2021-12-18 16:31:28 +0100

    add template
    Signed-off-by: Lunik <lunik@tiwabbit.fr>

---
+++ adventofcode/solutions/yXXXX/dXX/part2.py
@@ -0,0 +1,12 @@
+import os
+
+def main():
+  with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as file:
+    for line in file:
+      pass
+
+  return "TODO"
+
+
+if __name__ == "__main__":
+  print(main())
---
+++ adventofcode/tests/yXXXX/dXX/test_yXXXX_dXX.py
@@ -0,0 +1,12 @@
+from adventofcode.tests.utils.bench import calculate_duration
+
+from adventofcode.solutions.yXXXX.dXX.part1 import main as mainPart1
+from adventofcode.solutions.yXXXX.dXX.part2 import main as mainPart2
+
+
+def test_part1():
+  assert calculate_duration(mainPart1) == "TODO"
+
+
+def test_part2():
+  assert calculate_duration(mainPart2) == "TODO"
