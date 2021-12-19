120f4bee60e0ebdba0e87d3fe81cb0c14e50053b
Lunik <lunik@tiwabbit.fr>
2021-12-19 14:34:32 +0100

    2021/04
    Signed-off-by: Lunik <lunik@tiwabbit.fr>

--- adventofcode/solutions/y2021/d04/part2.py
+++ adventofcode/solutions/y2021/d04/part2.py
@@ -1,11 +1,31 @@
 import os
 
-def main():
-  with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r', encoding='UTF-8') as file:
-    for line in file:
-      print(line)
+from adventofcode.solutions.y2021.d04.part1 import parse, check_number, verify, calculate_solution
 
-  return "TODO"
+def main():
+
+  with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r', encoding='UTF-8') as file:
+    winning_numbers, grids = parse([line.strip() for line in file])
+
+  winning_grid = None
+  winning_number = None
+  for winning_number in winning_numbers:
+    for grid_index, grid in enumerate(grids):
+      check_number(grid, winning_number)
+      result = verify(grid)
+      if result is not None:
+        if len(grids) == 1:
+          winning_grid = grids[0]
+          break
+
+        grids[grid_index] = None
+
+    if winning_grid is not None:
+      break
+
+    grids = list(filter(None, grids))
+
+  return calculate_solution(winning_number, result, winning_grid)
 
 
 if __name__ == "__main__":
