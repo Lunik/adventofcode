4e9f77957b67f44cf9612bcb0d8685a729274514
Lunik <lunik@tiwabbit.fr>
2021-12-21 22:23:46 +0100

    2021/07
    Signed-off-by: Lunik <lunik@tiwabbit.fr>

---
+++ adventofcode/solutions/y2021/d07/part2.py
@@ -0,0 +1,19 @@
+import os
+import cProfile
+import pstats
+
+def main():
+  with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r', encoding='UTF-8') as file:
+    for line in file:
+      print(line)
+
+  return "TODO"
+
+
+if __name__ == "__main__":
+  with cProfile.Profile() as pr:
+    print(main())
+
+  stats = pstats.Stats(pr)
+  stats.sort_stats(pstats.SortKey.TIME)
+  stats.print_stats()
