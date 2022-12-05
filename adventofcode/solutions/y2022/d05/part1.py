12dcb05ef625ca41c9674c742b21e85cd4859ea6
Lunik <lunik@tiwabbit.fr>
2022-12-05 19:02:19 +0100

    2022/05
    Signed-off-by: Lunik <lunik@tiwabbit.fr>

---
+++ adventofcode/solutions/y2022/d05/part1.py
@@ -0,0 +1,82 @@
+import os
+import cProfile
+import pstats
+import re
+
+def parse(file):
+  part = 0
+
+  matrix = []
+  instructions = []
+
+  for line in file:
+    line = line.strip('\n')
+
+    if '1' in line and part == 0:
+      part += 1
+      continue
+
+    if line == '':
+      part += 1
+      continue
+
+    if part == 0:
+      x = 1
+      row = []
+      while x < len(line):
+        row.append(line[x] if line[x] != ' ' else None)
+        x += 4
+      matrix.append(row)
+
+    if part == 2:
+      res = re.match(r'move (\d+) from (\d+) to (\d+)', line)
+      instructions.append((int(res.group(1)), int(res.group(2)), int(res.group(3))))
+
+  new_matrix = []
+
+  for i in range(len(matrix[0])):
+    row = []
+    for j in range(len(matrix)):
+      row.append(matrix[j][i])
+
+    row.reverse()
+    row = list(filter(lambda x: x, row))
+    new_matrix.append(row)
+
+  return new_matrix, instructions
+
+def solve(matrix, instructions):
+
+  for count, from_tower, to_tower in instructions:
+    buffer = []
+    for x in range(count):
+      buffer.append(matrix[from_tower-1].pop())
+
+    matrix[to_tower-1] += buffer
+
+  res = ""
+
+  for column in matrix:
+    res += column.pop()
+
+  return res
+
+def print_matrix(matrix):
+  for row in matrix:
+    print(' '.join(row))
+
+
+def main():
+  with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r', encoding='UTF-8') as file:
+    data = parse(file)
+
+  return solve(*data)
+
+
+if __name__ == "__main__":
+  with cProfile.Profile() as pr:
+    print(main())
+
+  stats = pstats.Stats(pr)
+  stats.sort_stats(pstats.SortKey.TIME)
+  stats.print_stats()
---
+++ adventofcode/solutions/y2022/d05/part2.py
@@ -0,0 +1,38 @@
+import os
+import cProfile
+import pstats
+
+from adventofcode.solutions.y2022.d05.part1 import parse
+
+def solve(matrix, instructions):
+
+  for count, from_tower, to_tower in instructions:
+    buffer = []
+    for x in range(count):
+      buffer.append(matrix[from_tower-1].pop())
+
+    buffer.reverse()
+
+    matrix[to_tower-1] += buffer
+
+  res = ""
+
+  for column in matrix:
+    res += column.pop()
+
+  return res
+
+def main():
+  with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r', encoding='UTF-8') as file:
+    data = parse(file)
+
+  return solve(*data)
+
+
+if __name__ == "__main__":
+  with cProfile.Profile() as pr:
+    print(main())
+
+  stats = pstats.Stats(pr)
+  stats.sort_stats(pstats.SortKey.TIME)
+  stats.print_stats()
